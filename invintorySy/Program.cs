bb617ef2d39b1047ae23a290942b685d61c7decf
RAYMOND MCLEOD <ray.mcleod99@gmail.com>
2021-09-11 20:53:20 -0400

    Second commit First actual program

---
+++ invintorySy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace invintorySy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
